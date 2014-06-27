f874c337e0d0bb8cce8cfe6523c0d06c37b93198
Juan Gutierrez <juanny.gee@gmail.com>
2014-06-27 13:54:41 -0400

    add basic setup.py definition

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='SaasyDjangoRestFramework',
+    version='0.1dev',
+    packages=[],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    description="SaaS plugin for the django rest framework",
+)
