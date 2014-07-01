406540713de5e12dd712dc84accba46d16bdd9c2
Juan Gutierrez <juanny.gee@gmail.com>
2014-06-30 23:25:52 -0400

    flip the package name around a bit

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 """
-Setup file for SaasyDjangoRestFramework
+Setup file for DjangoRestFrameworkSaasy
 """
 from distutils.core import setup
 
 setup(
-    name='SaasyDjangoRestFramework',
+    name='DjangoRestFrameworkSaasy',
     version='0.1dev',
     packages=[],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
