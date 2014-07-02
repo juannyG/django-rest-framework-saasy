a7fd41f2acefcb5c776ae0c9a54b1b5a24761be9
Juan Gutierrez <juanny.gee@gmail.com>
2014-07-02 00:58:20 -0400

    define test command for setup.py; add placeholder test

---
+++ rest_framework_saasy/tests/test_routers.py
@@ -0,0 +1,9 @@
+"""SaaS router test suite"""
+import unittest
+
+
+class TestPlaceholder(unittest.TestCase):
+    """Placeholder"""
+    def test_x(self):
+        """Placeholder"""
+        self.assertEqual(1, 1)
--- setup.py
+++ setup.py
@@ -2,8 +2,45 @@
 Setup file for DjangoRestFrameworkSaasy
 """
 from distutils.core import setup
+from distutils.core import Command
+from unittest import TextTestRunner, TestLoader
+from glob import glob
+from os.path import splitext, basename, join as pjoin
+import os
+
+
+class TestCommand(Command):
+    """http://da44en.wordpress.com/2002/11/22/using-distutils/"""
+    user_options = []
+
+    def initialize_options(self):
+        self.root_prefix = 'rest_framework_saasy'
+        self._dir = '{}/{}'.format(os.getcwd(), self.root_prefix)
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        '''
+        Finds all the tests modules in tests/, and runs them.
+        '''
+        testfiles = []
+        for t in glob(pjoin(self._dir, 'tests', '*.py')):
+            if not t.endswith('__init__.py'):
+                testfiles.append('.'.join(
+                    [self.root_prefix,
+                     'tests',
+                     splitext(basename(t))[0]
+                     ])
+                )
+
+        print testfiles
+        tests = TestLoader().loadTestsFromNames(testfiles)
+        t_runner = TextTestRunner(verbosity=1)
+        t_runner.run(tests)
 
 setup(
+    cmdclass={'test': TestCommand},
     name='DjangoRestFrameworkSaasy',
     version='0.1dev',
     packages=[],
