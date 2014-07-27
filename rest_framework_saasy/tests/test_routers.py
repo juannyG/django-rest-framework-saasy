d4f7e8a459ae3416c64de9e17f778928c1d6f73d
Juan Gutierrez <juanny.gee@gmail.com>
2014-07-27 09:34:23 -0400

    add router test

--- rest_framework_saasy/tests/test_routers.py
+++ rest_framework_saasy/tests/test_routers.py
@@ -1,4 +1,26 @@
 """SaaS router test suite"""
+SAAS_CLIENT_MODEL = 'rest_framework_saasy.tests.test_routers.ClientModel'
+SAAS_CLIENT_MODULE = 'rest_framework_saasy.tests.client'
+
+import django.db.models.options as options
+options.DEFAULT_NAMES = options.DEFAULT_NAMES + ('saas_url_param',)
+
+from django.conf import settings
+from django.db import models
+settings.REST_FRAMEWORK['SAAS'] = {
+    'MODEL': SAAS_CLIENT_MODEL,
+    'MODULE': SAAS_CLIENT_MODULE
+    }
+
+
+class ClientModel(models.Model):
+    """Test client model"""
+    name = models.CharField(max_length=128)
+
+    class Meta:
+        """SaaS URL parameter defintion"""
+        saas_url_param = 'name'
+
 import unittest
 from rest_framework import viewsets
 from rest_framework.decorators import link, action
@@ -11,9 +33,6 @@ from rest_framework_saasy import viewsets as saas_viewsets
 factory = APIRequestFactory()
 
 urlpatterns = patterns('',)
-
-SAAS_CLIENT_MODEL = 'ClientModel'
-SAAS_CLIENT_MODULE = 'test.client.module'
 
 
 class BasicViewSet(saas_viewsets.ViewSetMixin, viewsets.ViewSet):
@@ -45,23 +64,17 @@ class BasicViewSet(saas_viewsets.ViewSetMixin, viewsets.ViewSet):
 class TestSimpleRouter(unittest.TestCase):
     """Placeholder"""
     def setUp(self):
-        from django.conf import settings
-        settings.REST_FRAMEWORK['SAAS'] = {
-            'MODEL': SAAS_CLIENT_MODEL,
-            'MODULE': SAAS_CLIENT_MODULE
-        }
-
         self.router = routers.SimpleRouter()
 
     def test_link_and_action_decorator(self):
         routes = self.router.get_routes(BasicViewSet)
-        decorator_routes = routes[2:]
+        decorator_routes = routes[9:]
         # Make sure all these endpoints exist and none have been clobbered
         for i, endpoint in enumerate(['action1', 'action2', 'action3', 'link1', 'link2']):
-            route = decorator_routes[i]
+            client_route = decorator_routes[i]
             # check url listing
-            self.assertEqual(route.url,
-                             '^{{prefix}}/{{lookup}}/{0}{{trailing_slash}}$'.format(endpoint))
+            self.assertEqual(client_route.url,
+                             '^(?P<name>\\w+)/{{prefix}}/{{lookup}}/{0}{{trailing_slash}}$'.format(endpoint))
             # check method to function mapping
             if endpoint == 'action3':
                 methods_map = ['post', 'delete']
@@ -70,4 +83,4 @@ class TestSimpleRouter(unittest.TestCase):
             else:
                 methods_map = ['get']
             for method in methods_map:
-                self.assertEqual(route.mapping[method], endpoint)
+                self.assertEqual(client_route.mapping[method], endpoint)
