509c1ef520ead729507751e96760cf8191887630
Juan Gutierrez <juanny.gee@gmail.com>
2014-08-24 23:09:54 -0400

    PyPI setup preparations

--- setup.py
+++ setup.py
@@ -1,12 +1,24 @@
 """Setup file for DjangoRestFrameworkSaasy"""
+import os
 from setuptools import setup, find_packages
+
+
+def read(fname):
+    """Utility function to read the README file.
+    Used for the long_description.  It's nice, because now 1) we have a top level
+    README file and 2) it's easier to type in the README file than to put a raw
+    string in below ..."""
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 setup(
-    name='DjangoRestFrameworkSaasy',
+    name='djangorestframework-saasy',
     version='0.1',
+    include_package_data=True,
     packages=find_packages(),
-    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    license="MIT",
     author="Juan Gutierrez",
     description="SaaS plugin for the django rest framework",
+    long_description=read('README.md'),
     url="https://github.com/juannyg/django-rest-framework-saasy",
     test_suite='rest_framework_saasy.runtests.runtests.main',
     install_requires=[
