c483bde02ce63d29140ff8cece46d120d2b9dacf
Juan Gutierrez <juanny.gee@gmail.com>
2015-08-10 14:16:10 -0400

    add unit tests for ClientMixin

---
+++ tests/test_client.py
@@ -0,0 +1,18 @@
+"""ClientMixin test cases"""
+from django.test import TestCase
+
+from rest_framework_saasy.client import ClientMixin
+
+
+class ClientMixinUnitTests(TestCase):
+    """ClientMixin unit tests"""
+    def setUp(self):
+        self.client_mixin = ClientMixin()
+
+    def test_saas_lookup_field(self):
+        with self.assertRaises(NotImplementedError):
+            self.client_mixin.saas_lookup_field
+
+    def test_saas_client_module(self):
+        with self.assertRaises(NotImplementedError):
+            self.client_mixin.saas_client_module('test_saas_url_kw')
